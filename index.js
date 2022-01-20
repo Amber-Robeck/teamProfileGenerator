40dddd75daf249deabe6e0bbd9f85c41fd8772a9
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-01-19 22:40:24 -0600

    added managerQuest to manager.js and removed manager questions from index.js

--- index.js
+++ index.js
@@ -1,68 +1,14 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const team = require("./src/page-template");
-const Manager = require("./lib/Manager");
+const { Manager, managerQuest } = require("./lib/Manager");
 const { Engineer, engineerQuest } = require("./lib/Engineer");
 const { Intern, internQuest } = require("./lib/Intern");
 let myTeam = [];
 
 
 const init = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the team manager's name?",
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the manager's name!");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "number",
-            name: "id",
-            message: "What is the team manager's ID number?",
-            validate: nameInput => {
-                if (isNaN(nameInput)) {
-                    console.log("Please enter a valid ID number!");
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "What is the team managers email address?",
-            validate: email => {
-                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-                if (valid) {
-                    return true;
-                } else {
-                    console.log('Please enter a valid email!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'number',
-            name: 'officeNumber',
-            message: "What is the team manager's office number?",
-            validate: nameInput => {
-                if (isNaN(nameInput)) {
-                    console.log('Please enter a valid office number!');
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        }
-    ])
+    inquirer.prompt(managerQuest)
         .then(managerInput => {
             const { name, id, email, officeNumber } = managerInput;
             const teamManager = new Manager(name, id, email, officeNumber);
